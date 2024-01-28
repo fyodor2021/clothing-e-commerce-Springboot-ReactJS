e899de03ed9a280c0efc51190090e3a57c633117
vedoor Barakat <vedoorbbs@gmail.com>
2024-01-27 19:23:58 -0500

    docker-compost.yml added to parent-microservice

---
+++ user-microservice/src/main/java/com/finefoods/usermicroservice/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.finefoods.usermicroservice.controller;
+
+import com.finefoods.usermicroservice.dto.UserRequest;
+import com.finefoods.usermicroservice.model.User;
+import com.finefoods.usermicroservice.service.UserServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+@Slf4j
+public class UserController {
+    private final UserServiceImpl userService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String createUser(@RequestBody UserRequest userRequest){
+       return userService.createUser(userRequest);
+    }
+
+    @PutMapping("/{userId}")
+    public String updateUser(@PathVariable("userId") String userId,
+                             @RequestBody UserRequest userRequest){
+        return userService.updateUser(userId,userRequest);
+
+    }
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<?> deleteUser(@PathVariable("userId") String userId){
+        userService.deleteUser(userId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @GetMapping("/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public User getUser(@PathVariable("userId") String userId){
+        return userService.getUser(userId);
+    }
+    @GetMapping("/role/{userId}")
+    public String getUserRole(@PathVariable("userId") String userId){
+        return userService.getUserRole(userId);
+    }
+
+}
---
+++ user-microservice/src/main/java/com/finefoods/usermicroservice/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.finefoods.usermicroservice.repository;
+
+import com.finefoods.usermicroservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByFnameAndLnameAndAddress(String fname,String lname, String address);
+}
